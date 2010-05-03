018563dc19e800a4edfca012d82d0ea61cc292e1
proffalken <theprofessor@threedrunkensysadsonthe.net>
2010-05-03 07:15:39 +0100

    updated mcollective init and added yumrepos module

--- mcollective/manifests/init.pp
+++ mcollective/manifests/init.pp
@@ -1,21 +1,40 @@
 #### Puppet module for MCollective ###
 
 class mcollective {
+
+	# ensure the correct repositories are set up
 	repository { "elff" }
 
+	# install the "common" package required for both client and server
 	package { "mcollective-common":
+		ensure => present,
+	}
+
+	# we need the rubgems installed as well.
+	package { "rubygem-stomp":
 		ensure => present,
 	}
 
 }
 
+# setup the class for the "server" element - this is required on all systems that you wish to monitor/control
 class mcollective::server inherits mcollective {
 	
-	package { "mcollective-server":
-		ensure => present,
-		require => File["mcollective-server.cfg"],
+	# push the server config out to the nodes
+	file {"mcollective-server.cfg":
+		source => "puppet:///modules/mcollective/server.cfg",
+		owner => "root",
+		group => "root",
+		mode => 440,
+		path => "/etc/mcollective/server.cfg",
 	}
 
+	# install the "server" package
+	package { "mcollective-server":
+		ensure => present,
+	}
+
+	# setup the service and ensure it has the relevant files and packages installed.
 	service {"mcollective":
 		ensure => present,
 		hasrestart => true,
@@ -23,3 +42,27 @@ class mcollective::server inherits mcollective {
 		subscribe => [Package["mcollective-server"],File["mcollective-server.cfg"]],
 	}
 }
+
+# the class for mcollective clients
+class mcollective::client {
+
+	# push the client cfg out to the nodes and subscribe to/require the package
+	file {"mcollective-client.cfg":
+		source => "puppet:///modules/mcollective/client.cfg",
+		path => "/etc/mcollective/client.cfg",
+		owner => "root",
+		group => "root",
+		mode => 440
+		path => "/etc/mcollective/client.cfg",
+		require => Package["mcollective-client"],
+		subscribe => Package["mcollective-client"],
+	}
+
+	# install the package
+	package { "mcollective-client":
+		ensure => present,
+		}
+
+}
+	
+	
---
+++ yumrepositories/manifests/init.pp
@@ -0,0 +1,1 @@
+# class to manage yum repos
