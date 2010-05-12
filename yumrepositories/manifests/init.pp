c8fa989a39e9386eaec9703078f0ac5b7b9d44ee
proffalken <theprofessor@threedrunkensysadsonthe.net>
2010-05-12 20:02:59 +0100

    Added GPL License to the project

--- yumrepositories/manifests/init.pp
+++ yumrepositories/manifests/init.pp
@@ -1,3 +1,17 @@
+# The puppet modules contained in this package are Copyright (c) 2010 Matthew Macdonald-Wallace and are released under the GPL
+# 
+# "Professor Falken's Puppet Modules" is free software: you can redistribute it and/or modify 
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# "Professor Falken's Puppet Modules" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with "Professor Falken's Puppet Modules".  If not, see <http://www.gnu.org/licenses/>.
 # class to manage yum repos
 
 # define the class with a noop to load it
